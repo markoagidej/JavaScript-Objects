e14d25cf3c089fbd512ed98786abc91d702ea0c6
Marko <markogidej@yahoo.com>
2024-06-24 12:14:49 -0400

    Init Commit

---
+++ math.js
@@ -0,0 +1,7 @@
+// Task 1: Create a constructor function for the Account object with properties for accountNumber, balance, and owner.
+
+
+// Task 2: Implement methods within the Account object to deposit and withdraw funds.
+
+
+// Task 3: Create a method to calculate compound interest based on the balance and specified interest rate.
